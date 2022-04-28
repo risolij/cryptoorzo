c1737593a020d238b3a8475fe281eeeb14b7876d
risolij <risolij@gmail.com>
2022-04-28 14:50:05 -0700

    modifying story card styles

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,14 +1,28 @@
 import Card from 'react-bootstrap/Card';
 import '../styles/article.css';
+import { HandThumbsUp, HandThumbsDown, Twitter } from 'react-bootstrap-icons';
 
 export default function NewsItem(props) {
+    let handleClick = () => {
+        console.log("clicked");
+    }
+
     return (
         <Card className="news_story" bg="light">
             <Card.Img variant="top" className="news_story_img" src={props.newsItem.imgURL} />
+            <Card.Header bg="dark" className="news_story_header"><a href={props.newsItem.link}>{props.newsItem.title}</a></Card.Header>
             <Card.Body className="news_story_body">
-            <Card.Text>{props.newsItem.description}</Card.Text>
+                <Card.Text>{props.newsItem.description}</Card.Text>
             </Card.Body>
-            <Card.Footer bg="dark" className="news_story_footer"><a href={props.newsItem.link}>{props.newsItem.title}</a></Card.Footer>
+            <Card.Footer className="news_story_footer">
+                <div>
+                    <Twitter />
+                </div>
+                <div>
+                    <Card.Text><HandThumbsUp onClick={handleClick}/>{props.newsItem.reactionsCount[2]}</Card.Text>
+                    <Card.Text><HandThumbsDown />{props.newsItem.reactionsCount[3]}</Card.Text>
+                </div>
+            </Card.Footer>
         </Card>
     )
 }
