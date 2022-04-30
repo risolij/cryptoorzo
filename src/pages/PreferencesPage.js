41c0c911a51df536efc330ca51fb9e91ac284718
risolij <risolij@gmail.com>
2022-04-30 15:49:53 -0700

    Implementing basic filter functionality

--- src/pages/PreferencesPage.js
+++ src/pages/PreferencesPage.js
@@ -1,10 +1,12 @@
 import NavComponent from '../components/NavComponent';
 import CondCryptoList from '../components/CondCryptoList';
+import SearchBar from '../components/SearchBar';
 
 export default function PreferencesPage() {
     return (
         <>
             <NavComponent />
+            <SearchBar />
             <CondCryptoList />
         </>
 
